474e72aa07852b08bddbf5d9d7a42273eb1bd36b
João Nogueira <joaoprnogueira@gmail.com>
2021-02-20 21:26:57 +0000

    Remove projects

--- src/components/index.js
+++ src/components/index.js
@@ -11,5 +11,4 @@ export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
 export { default as Jobs } from './sections/jobs';
 export { default as Education } from './sections/education';
-export { default as Projects } from './sections/projects';
 export { default as Contact } from './sections/contact';
